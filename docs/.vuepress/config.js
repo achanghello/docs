d96fdb86159c6a8a35ae3135c482dccc2d4ca063
tom <1431453805@qq.com>
2022-07-04 17:17:49 +0800

    feat: pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,14 +8,30 @@ module.exports = {
   head: [
     ['meta', { name: 'author', content: '青鸟飞鱼' }],
     ['meta', { name: 'Keywords', content: '青鸟飞鱼各自生活永不相见' }],
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
     [
-      '@vuepress/last-updated',
-      {
+      '@vuepress/last-updated', {
         transformer: (timestamp) => {
           return moment(timestamp).format("LLLL")
+        }
+      }
+    ],
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "新的风暴已经出现",
+          buttonText: "刷新"
         }
       }
     ]
